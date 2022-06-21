cfb95552485b3277501086e1827e4ecf4c3c2f62
estronnom <estronnom@gmail.com>
2022-06-21 22:43:36 +0300

    DDL done, queries added

--- query.sql
+++ query.sql
@@ -1,10 +1,14 @@
-EXPLAIN ANALYZE SELECT * FROM category WHERE parent_id = 619;
+--Найти количество дочерних элементов первого уровня вложенности для категорий номенклатуры
+--Adjacency List, id=619
+SELECT * FROM category WHERE parent_id = 619;
 
-EXPLAIN ANALYZE SELECT * FROM category WHERE path[:4] = ARRAY[3, 2, 1, 5] AND cardinality(path) = 5;
+--Найти количество дочерних элементов первого уровня вложенности для категорий номенклатуры
+--Materialized Path, id=619
+SELECT * FROM category WHERE path[:4] = ARRAY[3, 2, 1, 5] AND cardinality(path) = 5;
 
-EXPLAIN ANALYZE SELECT * FROM category WHERE path[:3] = ARRAY[1, 1, 1] AND cardinality(path) > 3;
-
-EXPLAIN ANALYZE WITH RECURSIVE nodes(id, parent_id, path, name) AS (
+--Найти все древо подкатегорий для категории
+--Adjacency List, id=42
+WITH RECURSIVE nodes(id, parent_id, path, name) AS (
     SELECT s1.category_id, s1.name, s1.parent_id, s1.path
     FROM category s1 WHERE parent_id = 42
         UNION
@@ -12,4 +16,15 @@ EXPLAIN ANALYZE WITH RECURSIVE nodes(id, parent_id, path, name) AS (
     FROM category s2, nodes s1 WHERE s2.parent_id = s1.id
 )
 SELECT * FROM nodes
-order by parent_id;
+ORDER BY parent_id;
+
+--Найти все древо подкатегорий для категории
+--Materialized Path, id=42
+SELECT * FROM category WHERE path[:3] = ARRAY[1, 1, 1] AND cardinality(path) > 3;
+
+--Получение информации о сумме товаров заказанных под каждого клиента
+SELECT c.name, SUM(o.order_sum)
+FROM client c
+JOIN "order" o USING(client_id)
+GROUP BY c.name
+ORDER BY 2 DESC;
--- schema.sql
+++ schema.sql
@@ -6,5 +6,38 @@ CREATE TABLE IF NOT EXISTS category(
     PRIMARY KEY(category_id)
 );
 
-CREATE INDEX parent_id_btree ON category USING btree(parent_id);
+CREATE INDEX IF NOT EXISTS parent_id_btree ON category USING btree(parent_id);
 --CREATE INDEX path_gin ON category USING gin(path);
+
+CREATE TABLE IF NOT EXISTS good(
+  good_id SERIAL,
+  category_id INTEGER REFERENCES category(category_id) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  price REAL,
+  quantity INTEGER,
+  PRIMARY KEY(good_id)
+);
+
+CREATE TABLE IF NOT EXISTS client(
+  client_id SERIAL,
+  name VARCHAR(64) NOT NULL,
+  client_address VARCHAR(512),
+  client_phone VARCHAR(16),
+  PRIMARY KEY(client_id)
+);
+
+CREATE TABLE IF NOT EXISTS "order"(
+  order_id SERIAL,
+  client_id INTEGER REFERENCES client(client_id) NOT NULL,
+  order_sum REAL,
+  order_address VARCHAR(512),
+  order_phone VARCHAR(16),
+  PRIMARY KEY(order_id)
+);
+
+CREATE TABLE IF NOT EXISTS good_order(
+    good_order_id SERIAL,
+    good_id INTEGER REFERENCES good(good_id) NOT NULL,
+    order_id INTEGER REFERENCES "order"(order_id) NOT NULL,
+    PRIMARY KEY(good_order_id)
+);
