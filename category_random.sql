41c82f3aa328f412f8262967a234c441e0e5567c
estronnom <estronnom@gmail.com>
2022-06-21 18:41:15 +0300

    procedure fixed, basic explain analyze queries added

--- category_random.sql
+++ category_random.sql
@@ -1,22 +1,25 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE OR REPLACE procedure populate_category(a INTEGER)
 AS $$
+DECLARE
+    random_id INTEGER;
 BEGIN
+    INSERT INTO category(path, name) VALUES
+    (ARRAY[1], 'FIRST'),
+    (ARRAY[2], 'SECOND'),
+    (ARRAY[3], 'THIRD');
     FOR x in 1..a LOOP
-        WITH random_id AS (
-            SELECT random() * (SELECT MAX(category_id) FROM category)::INTEGER + 1 id
-        )
-
+        SELECT random() * (SELECT MAX(category_id) FROM category)::INTEGER + 1 id
+        INTO random_id;
         INSERT INTO category(parent_id, path, name)
-        SELECT
-            random_id.id,
-            COALESCE(
-                    ((SELECT path FROM category WHERE category_id = random_id.id) ||
-                    (SELECT COUNT(path) FROM category WHERE parent_id = random_id.id) + 1),
-                ARRAY[random_id.id]),
-            encode(gen_random_bytes(18), 'base64')
-        FROM random_id;
+            SELECT
+                random_id,
+                array_append((SELECT path FROM category WHERE category_id = random_id),
+                    (SELECT COUNT(path) FROM category WHERE parent_id = random_id) + 1),
+                encode(gen_random_bytes(18), 'base64');
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 
-
+CALL populate_category(100);
--- schema.sql
+++ schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE category(
+CREATE TABLE IF NOT EXISTS category(
     category_id SERIAL,
     parent_id INTEGER REFERENCES category(category_id),
     path INTEGER[],
     name VARCHAR(128),
     PRIMARY KEY(category_id)
-)
+);
+
+CREATE INDEX parent_id_btree ON category USING btree(parent_id);
+--CREATE INDEX path_gin ON category USING gin(path);
